8bc942ffe0a5785739855b5025e1eebe72079985
Ricardo Brites <ricardo.brites.work@gmail.com>
2022-04-12 17:50:32 +0100

    Disabled the Default HUD class

--- Source/Portal_GE_II/Portal_GE_IIGameMode.cpp
+++ Source/Portal_GE_II/Portal_GE_IIGameMode.cpp
@@ -5,13 +5,9 @@
 #include "Portal_GE_IICharacter.h"
 #include "UObject/ConstructorHelpers.h"
 
-APortal_GE_IIGameMode::APortal_GE_IIGameMode()
-	: Super()
+APortal_GE_IIGameMode::APortal_GE_IIGameMode() : Super()
 {
 	// set default pawn class to our Blueprinted character
 	static ConstructorHelpers::FClassFinder<APawn> PlayerPawnClassFinder(TEXT("/Game/FirstPersonCPP/Blueprints/FirstPersonCharacter"));
 	DefaultPawnClass = PlayerPawnClassFinder.Class;
-
-	// use our custom HUD class
-	HUDClass = APortal_GE_IIHUD::StaticClass();
 }
