68b9a7708082607c41d9e924fd1a9a46eae71530
Ricardo Brites <ricardo.brites.work@gmail.com>
2022-06-25 23:31:46 +0100

    Plates weapon color, give weapon prep
    The platforms now have different colors based on what type of weapon they offer
    the platforms now are ready to call a server function from the character to give it a weapon (plate specific)

--- Source/Portal_GE_II/Private/PortalGameMode.cpp
+++ Source/Portal_GE_II/Private/PortalGameMode.cpp
@@ -3,5 +3,29 @@
 
 #include "PortalGameMode.h"
 
+class UGameplayStatics;
 
+void APortalGameMode::SR_SpawnWeaponInPlayer(TSubclassOf<APortal_GE_IICharacter> characterRef,int32 weaponType)
+{
+	////// Create a gun mesh component
+	//characterRef->FP_Gun = characterRef.CreateDefaultSubobject<USkeletalMeshComponent>(TEXT("FP_Gun"));
+	//asCharacter.FP_Gun->SetOnlyOwnerSee(true);			// otherwise won't be visible in the multiplayer
+	//asCharacter.FP_Gun->bCastDynamicShadow = false;
+	//asCharacter.FP_Gun->CastShadow = false;
+	//asCharacter.FP_Gun->SetupAttachment(asCharacter.GetMesh1P(), TEXT("GripPoint"));
+	//asCharacter.FP_Gun->SetupAttachment(asCharacter.GetRootComponent());
 
+	////allows the character to shoot again
+	//asCharacter.SetCanShoot(true);
+
+	////pass the weapon type to the gun
+	//asCharacter.SetWeaponType(weaponType);
+
+	////trigger a color change in the 3p gun mesh
+	GEngine->AddOnScreenDebugMessage(-1, 15.0f, FColor::Yellow, TEXT("Some debug message!"));
+}
+
+FLinearColor APortalGameMode::GetWeaponColor(int32 weaponType)
+{
+	return lcWeaponColor[weaponType];
+}
--- Source/Portal_GE_II/Public/PortalGameMode.h
+++ Source/Portal_GE_II/Public/PortalGameMode.h
@@ -4,8 +4,13 @@
 
 #include "CoreMinimal.h"
 #include "GameFramework/GameMode.h"
+
+#include "../Portal_GE_IICharacter.h"
+#include "././Kismet/GameplayStatics.h"
+
 #include "PortalGameMode.generated.h"
 
+class APortal_GE_IICharacter;
 /**
  * 
  */
@@ -23,5 +28,21 @@ private:
 		UPROPERTY(EditDefaultsOnly, category = "Weapon Properties", meta = (AllowPrivateAccess = true))
 			TArray<float> fWeaponCooldown;
 		UPROPERTY(EditDefaultsOnly, category = "Weapon Properties", meta = (AllowPrivateAccess = true))
-			TArray<int16> iWeaponAmmo;
+			TArray<int32> iWeaponAmmo;
+
+
+		
+public:
+	/*
+	* weapon type: 
+	*	0-> Assault Rifle
+	*	1-> Shotgun
+	*	2-> Rocket Launcher
+	*	3-> Portal Gun
+	*/
+	UFUNCTION( BlueprintCallable )
+	void SR_SpawnWeaponInPlayer(TSubclassOf<APortal_GE_IICharacter> characterRef, int32 weaponType);
+	
+	FLinearColor GetWeaponColor(int32 weaponType);
+
 };
